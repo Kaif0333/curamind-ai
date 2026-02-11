a00821c0ee8b5f5c23601ad0e5283a539daf5f42
Kaif0333 <smohammedkaifbasha@gmail.com>
2026-02-11 19:43:33 +0530

    Add patient appointment creation flow

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import patient_dashboard, doctor_dashboard
+from .views import patient_dashboard, create_appointment
 
 urlpatterns = [
     path("patient/", patient_dashboard, name="patient_dashboard"),
-    path("doctor/", doctor_dashboard, name="doctor_dashboard"),
+    path("patient/appointment/", create_appointment, name="create_appointment"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,19 +1,41 @@
-from django.shortcuts import render
-from django.http import HttpResponseForbidden
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
+from .models import User, Appointment
 
 
 @login_required
 def patient_dashboard(request):
-    if request.user.role != "patient":
-        return HttpResponseForbidden("You are not allowed to access this page")
+    if request.user.user_type != "patient":
+        return HttpResponseForbidden("You are not allowed")
 
-    return render(request, "users/patient_dashboard.html")
+    appointments = Appointment.objects.filter(patient=request.user)
+    return render(request, "users/patient_dashboard.html", {
+        "appointments": appointments
+    })
 
 
 @login_required
-def doctor_dashboard(request):
-    if request.user.role != "doctor":
-        return HttpResponseForbidden("You are not allowed to access this page")
+def create_appointment(request):
+    if request.user.user_type != "patient":
+        return HttpResponseForbidden("You are not allowed")
 
-    return render(request, "users/doctor_dashboard.html")
+    doctors = User.objects.filter(user_type="doctor")
+
+    if request.method == "POST":
+        doctor_id = request.POST.get("doctor")
+        date = request.POST.get("date")
+        time = request.POST.get("time")
+
+        Appointment.objects.create(
+            patient=request.user,
+            doctor_id=doctor_id,
+            date=date,
+            time=time
+        )
+
+        return redirect("patient_dashboard")
+
+    return render(request, "users/create_appointment.html", {
+        "doctors": doctors
+    })
